6f25d30cdfd8c4f2edbcaa4907954363d479068f
em4n0n <58372945+em4n0n@users.noreply.github.com>
2024-10-14 21:42:09 -0700

    Create MenuItemsView class and migrate

--- LittleLemon/LittleLemonAPI/views.py
+++ LittleLemon/LittleLemonAPI/views.py
@@ -1,5 +1,8 @@
 from rest_framework import generics
-from .models. import MenuItem
+from .models import MenuItem
 from .serializers import MenuItemSerializer
 
 # Create your views here.
+class MenuItemsView(generics.ListCreateAPIView):
+    queryset = MenuItem.objects.all() # Retrieve all records
+    serializer_class = MenuItemSerializer # To display and store the record
